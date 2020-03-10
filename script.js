b4caf529cabe9f13bf04c7971c4d2e32d247b0af
Vladislav <w3k5@yandex.ru>
2020-03-10 17:20:11 +0300

    Выполнены задания по всем типам циклов. Успешно проходит проверка, если отправляются пустые инпуты. Не реализована проверка, если answer == NaN, требуется вернуться к этому позже

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 let money = +prompt("Ваш бюджет на месяц?");
-console.log(money);
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 const appData = {
@@ -11,37 +10,89 @@ const appData = {
   income: [],
   savings: false
 };
+// вариант с for
 
-function checkCosts() {
-  for (let i = 0; i < 2; i++) {
-    console.log("i = " + i + " в начале цикла");
-    let question = prompt(
-      "Введите обязательную статью расходов в этом месяце",
-      ""
-    );
-    let answer = +prompt("Во сколько обойдется?", "");
-    if (
-      question != "" &&
-      answer != "" &&
-      typeof question != null &&
-      typeof answer != null
-    ) {
-      console.log("done");
-      appData.expenses[question] = answer;
-      console.log(i);
-    } else {
-      console.error("Данные введены некорректно!");
-      console.log("i = " + i + " в конце цикла");
-      return i, checkCosts();
-    }
+for (let i = 0; i < 2; i++) {
+  let question = prompt(
+    "Введите обязательную статью расходов в этом месяце",
+    ""
+  );
+  let answer = +prompt("Во сколько обойдется?", "");
+  if (
+    question != "" &&
+    typeof question != null &&
+    answer != "" &&
+    answer != NaN &&
+    // не срабатывает проверка с NaN, надо вернуться к этому чуть позже
+    typeof answer != null
+  ) {
+    console.log("done");
+    appData.expenses[question] = answer;
+  } else if (question == "" || typeof question === null) {
+    i--;
+    alert("Неверная задача! повторите ввод!");
+    console.error("Данные введены некорректно!");
+  } else if (answer == "" || typeof answer === null) {
+    i--;
+    alert("Неверное значение! Повторите ввод!");
+    console.error("Данные введены некорректно!");
   }
 }
 
-checkCosts();
+// вариант с while
+// let i = 0;
+// while (i < 2) {
+//   let question = prompt(
+//     "Введите обязательную статью расходов в этом месяце",
+//     ""
+//   );
+//   let answer = +prompt("Во сколько обойдется?", "");
+//   if (
+//     question != "" &&
+//     answer != "" &&
+//     typeof question != null &&
+//     typeof answer != null
+//   ) {
+//     console.log("done");
+//     appData.expenses[question] = answer;
+//   } else {
+//     i--;
+//     console.error("Данные введены некорректно!");
+//   }
+
+//   i++;
+//   console.log(i);
+// }
+
+//Вариант с do while
+// let i = 0;
+// do {
+//   let question = prompt(
+//     "Введите обязательную статью расходов в этом месяце",
+//     ""
+//   );
+//   let answer = +prompt("Во сколько обойдется?", "");
+//   if (
+//     question != "" &&
+//     answer != "" &&
+//     typeof question != null &&
+//     typeof answer != null
+//   ) {
+//     console.log("done");
+//     appData.expenses[question] = answer;
+//   } else {
+//     i--;
+//     console.error("Данные введены некорректно!");
+//   }
+
+//   i++;
+//   console.log(i);
+// } while (i < 2);
 
 appData.moneyPerDay = Math.round(appData.budjet / 30);
 alert("Ежедневный бюджет " + appData.moneyPerDay);
 
+//Требуется использовать <= вместо < т.к. если бюджет 3000, то moneyPerDay == 100, и происходит ошибка.
 if (appData.moneyPerDay <= 100) {
   console.log("Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
