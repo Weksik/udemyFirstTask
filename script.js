694af79ccce7f7f10efcea6ea19efb6a826889e9
Vladislav <w3k5@yandex.ru>
2020-03-10 15:53:25 +0300

    Начала модификации кода, добавление функции, которая начинает цикл заного, если поля введены неверно. В данный момент ошибка, если условие в цикле не выполняется, то цикл сбрасывается полностью и начинается сначала, из-за чего можно добавить более двух объектов.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 let money = +prompt("Ваш бюджет на месяц?");
+console.log(money);
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 const appData = {
@@ -11,24 +12,39 @@ const appData = {
   savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-  let question = prompt(
-    "Введите обязательную статью расходов в этом месяце",
-    ""
-  );
-  let answer = +prompt("Во сколько обойдется?", "");
-  if (question != "" && answer != "") {
-    console.log("done");
-    appData.expenses[question] = answer;
+function checkCosts() {
+  for (let i = 0; i < 2; i++) {
+    console.log("i = " + i + " в начале цикла");
+    let question = prompt(
+      "Введите обязательную статью расходов в этом месяце",
+      ""
+    );
+    let answer = +prompt("Во сколько обойдется?", "");
+    if (
+      question != "" &&
+      answer != "" &&
+      typeof question != null &&
+      typeof answer != null
+    ) {
+      console.log("done");
+      appData.expenses[question] = answer;
+      console.log(i);
+    } else {
+      console.error("Данные введены некорректно!");
+      console.log("i = " + i + " в конце цикла");
+      return i, checkCosts();
+    }
   }
 }
+
+checkCosts();
 
 appData.moneyPerDay = Math.round(appData.budjet / 30);
 alert("Ежедневный бюджет " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
+if (appData.moneyPerDay <= 100) {
   console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
   console.log("Средний уровень достатка");
 } else if (appData.moneyPerDay > 2000) {
   console.log("Высокий уровень достатка");
